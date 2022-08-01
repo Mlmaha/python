04fe3e2f35e711ea0e6aa902af31053a887af4fa
Mlmaha <mlmaha8@gmail.com>
2022-08-01 10:14:54 +0530

    tips to work with json data

--- learning.py
+++ learning.py
@@ -1,1 +1,27 @@
+#tips to work with json data
+import json
+response = {"entries": [  { "id" : "yy"}, {"id" : "xx"} ]} #dict
 
+print(type(response))
+
+print("---entries list--")
+entries_list =response['entries']
+print(entries_list)
+
+print(type(entries_list)) #list
+
+for entry in entries_list:
+    print(entry['id']) #dict
+
+
+#String to json
+emp = '{"name": "Maha", "id": 1, "city": "yy"}' #string
+emp_json = json.loads(emp)
+print(emp_json)
+print(type(emp_json)) #dict
+
+#json to string
+payload = {'aa': 3} #dict
+string_output = json.dumps(payload)
+print(type(string_output)) #string
+print(string_output)
